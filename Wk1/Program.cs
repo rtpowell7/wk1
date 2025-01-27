e63a9225fd8582ff9a8f3e1265c4861b91120559
rtpowell7 <rtpowell22@amarillocollege.com>
2025-01-26 22:30:33 -0600

    Update Program.cs

--- Wk1/Program.cs
+++ Wk1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,25 @@ namespace Wk1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to ITSE1302");
+
+            //Excersiese 2
+            //Get firstName
+            Console.Write("What is your first name: ");
+            string fname = Console.ReadLine();
+            //Get lastname
+            Console.Write("What is your last name: ");
+            string lname = Console.ReadLine();
+            //Get age
+            Console.Write("What is your age: ");
+            int age = int.Parse(Console.ReadLine());
+            //Get color
+            Console.Write("Enter your favorite color: ");
+            string color = Console.ReadLine();
+
+            // Print statement
+            Console.WriteLine($"Hello {fname}{lname}, you are {age} years old and your favorite color is {color}.");
+
         }
     }
 }
